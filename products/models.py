333c89cb7ed1b414890d1dc9e9f54cb674d509f6
lukaszkukla <lukaszkukla@gmail.com>
2022-10-22 16:32:32 +0000

    Build: add products app, models and upload initial data set

--- products/models.py
+++ products/models.py
@@ -1,3 +1,47 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    """
+    Category class
+    """
+    class Meta:
+        """
+        Meta class for Category
+        """
+        verbose_name_plural = 'Categories'
+    name = models.CharField(max_length=254)
+    friendly_name = models.CharField(max_length=254, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+    def get_friendly_name(self):
+        """
+        Returns friendly name
+        """
+        return self.friendly_name
+
+
+class Product(models.Model):
+    """
+    Product class
+    """
+    category = models.ForeignKey(
+        'Category',
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL
+    )
+    sku = models.CharField(max_length=254, null=True, blank=True)
+    name = models.CharField(max_length=254)
+    description = models.TextField()
+    has_sizes = models.BooleanField(default=False, null=True, blank=True)
+    price = models.DecimalField(max_digits=10 ,decimal_places=2)
+    rating = models.DecimalField(
+        max_digits=3, decimal_places=2, null=True, blank=True)
+    image_url = models.URLField(max_length=1024, null=True, blank=True)
+    image = models.ImageField(null=True, blank=True)
+
+    def __str__(self):
+        return self.name
