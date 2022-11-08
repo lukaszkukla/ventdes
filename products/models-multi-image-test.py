8fd6a5d0a2b4c7fc6fb16eca7ab1e7095dfdb4ad
lukaszkukla <lukaszkukla@gmail.com>
2022-11-08 22:20:11 +0000

    Feat: add product quantity selector

---
+++ products/models-multi-image-test.py
@@ -0,0 +1,66 @@
+from django.db import models
+from django.core.validators import MinLengthValidator
+
+
+class Category(models.Model):
+    """
+    Category class
+    """
+    class Meta:
+        """
+        Meta class for Category
+        """
+        verbose_name_plural = 'Categories'
+    name = models.CharField(max_length=254)
+    friendly_name = models.CharField(max_length=254, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+    def get_friendly_name(self):
+        """
+        Returns friendly name
+        """
+        return self.friendly_name
+
+
+class Product(models.Model):
+    """
+    Product class
+    """
+    category = models.ForeignKey(
+        'Category',
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL
+    )
+    sku = models.CharField(max_length=254, null=True, blank=True)
+    name = models.CharField(max_length=254)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    rating = models.DecimalField(
+        max_digits=3, decimal_places=2, null=True, blank=True
+    )
+    image = models.ForeignKey(
+        'ProductImage', null=True, blank=True, on_delete=models.SET_NULL
+    )
+
+    def __str__(self):
+        return self.name
+
+
+class ProductImage(models.Model):
+    """
+    ProductImage class
+    """
+    item = models.ForeignKey(
+        'Product', null=True, blank=True, on_delete=models.SET_NULL
+    )
+    title = models.CharField(
+        max_length=254,
+        null=True,
+        unique=True,
+        validators=[MinLengthValidator(3)]
+    )
+    image_url = models.URLField(max_length=1024, null=True, blank=True)
+    image = models.ImageField(null=True, blank=True)
