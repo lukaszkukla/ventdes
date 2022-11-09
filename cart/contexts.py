9ff2e6ecf607b1642472996934c6848131bfd7d7
lukaszkukla <lukaszkukla@gmail.com>
2022-11-09 21:55:36 +0000

    Build: update cart context processor

--- cart/contexts.py
+++ cart/contexts.py
@@ -1,5 +1,7 @@
 from decimal import Decimal
 from django.conf import settings
+from django.shortcuts import get_object_or_404
+from products.models import Product
 
 from ventdes.settings import FREE_DELIVERY_THRESHOLD
 
@@ -9,8 +11,19 @@ def cart_contents(request):
     cart_items = []
     total = 0
     product_count = 0
+    cart = request.session.get('cart', {})
     free_delivery_threshold = settings.FREE_DELIVERY_THRESHOLD
     standard_delivery_percentage = settings.STANDARD_DELIVERY_PERCENTAGE
+
+    for item_id, quantity in cart.items():
+        product = get_object_or_404(Product, pk=item_id)
+        total += quantity * product.price
+        product_count += quantity
+        cart_items.append({
+            'item_id': item_id,
+            'quantity': quantity,
+            'product': product,
+        })
 
     if total < free_delivery_threshold:
         delivery = total * Decimal(standard_delivery_percentage / 100)
