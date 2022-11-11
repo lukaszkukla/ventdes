9645cabb83db15ed168a3cdd687ed7c9715c5b10
lukaszkukla <lukaszkukla@gmail.com>
2022-11-11 00:07:47 +0000

    Build: add adjust bag and remove from bag views, and calc subtotal template filter

--- cart/contexts.py
+++ cart/contexts.py
@@ -25,17 +25,6 @@ def cart_contents(request):
                 'quantity': item_data,
                 'product': product,
             })
-        else:
-            product = get_object_or_404(Product, pk=item_id)
-            for size, quantity in item_data['items_by_size'].items():
-                total += quantity * product.price
-                product_count += quantity
-                cart_items.append({
-                    'item_id': item_id,
-                    'quantity': quantity,
-                    'product': product,
-                    'size': size,
-                })
 
     if total < free_delivery_threshold:
         delivery = total * Decimal(standard_delivery_percentage / 100)
