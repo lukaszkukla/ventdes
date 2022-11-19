0fa863b7ee210d45d80ebfee5e826dc8a2d3848d
lukaszkukla <lukaszkukla@gmail.com>
2022-11-19 23:52:04 +0000

    Build: cart model

--- ventdes/settings.py
+++ ventdes/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['ventdes.herokuapp.com', 'localhost', '127.0.0.1']
 
@@ -131,6 +131,7 @@ WSGI_APPLICATION = 'ventdes.wsgi.application'
 #         'NAME': BASE_DIR / 'db.sqlite3',
 #     }
 # }
+
 
 if 'DATABASE_URL' in os.environ:
     DATABASES = {
