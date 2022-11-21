9b95928bedd0d58ec845c55653f30493f2206964
lukaszkukla <lukaszkukla@gmail.com>
2022-11-21 21:28:59 +0000

    Fix: image upload AWS S3 bucket

--- products/views.py
+++ products/views.py
@@ -79,7 +79,7 @@ def add_product(request):
     if request.method == 'POST':
         form = ProductForm(request.POST, request.FILES)
         if form.is_valid():
-            form.save()
+            product = form.save()
             messages.success(
                 request, f'Successfully added product {product.name}!'
             )
