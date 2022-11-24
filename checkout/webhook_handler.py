1cfbbc654155a2cc5821914b76435458e71fc8e1
lukaszkukla <lukaszkukla@gmail.com>
2022-11-24 21:46:56 +0000

    Fix: Stripe payment

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -47,19 +47,28 @@ class StripeWH_Handler:
         """
         Handle the payment_intent.succeeded webhook from Stripe
         """
+        # intent = event.data.object
+        # pid = intent.id
+        # cart = intent.metadata.cart
+        # save_info = intent.metadata.save_info
+
+        # # Get the Charge object
+        # stripe_charge = stripe.Charge.retrieve(
+        #     intent.latest_charge
+        # )
+
+        # billing_details = stripe_charge.billing_details
+        # shipping_details = intent.shipping
+        # grand_total = round(stripe_charge.amount / 100, 2)
+
         intent = event.data.object
         pid = intent.id
-        cart = intent.metadata.cart
+        bag = intent.metadata.bag
         save_info = intent.metadata.save_info
 
-        # Get the Charge object
-        stripe_charge = stripe.Charge.retrieve(
-            intent.latest_charge
-        )
-
-        billing_details = stripe_charge.billing_details
+        billing_details = intent.charges.data[0].billing_details
         shipping_details = intent.shipping
-        grand_total = round(stripe_charge.amount / 100, 2)
+        grand_total = round(intent.charges.data[0].amount / 100, 2)
 
         # Clean data in the shipping details
         for field, value in shipping_details.address.items():
