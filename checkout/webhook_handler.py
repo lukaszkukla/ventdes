855bc2dc1b98061d0d331f07d63e1e36ec953d2e
lukaszkukla <lukaszkukla@gmail.com>
2022-11-24 20:58:24 +0000

    Fix: Stripe payments

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -7,7 +7,6 @@ from .models import Order, OrderLineItem
 from products.models import Product
 from profiles.models import UserProfile
 
-import stripe
 import json
 import time
 
@@ -52,14 +51,9 @@ class StripeWH_Handler:
         cart = intent.metadata.cart
         save_info = intent.metadata.save_info
 
-        # Get the Charge object
-        stripe_charge = stripe.Charge.retrieve(
-            intent.latest_charge
-        )
-
-        billing_details = stripe_charge.billing_details
+        billing_details = intent.charges.data[0].billing_details
         shipping_details = intent.shipping
-        grand_total = round(stripe_charge.amount / 100, 2)
+        grand_total = round(intent.charges.data[0].amount / 100, 2)
 
         # Clean data in the shipping details
         for field, value in shipping_details.address.items():
