7a20c6ef515d2fe85be07c7eba261342a95085a2
lukaszkukla <lukaszkukla@gmail.com>
2022-11-25 20:20:00 +0000

    Feat: add Newsletter app

---
+++ newsletter/admin.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from .models import SubscribeNewsletter
+
+
+class SubscribeNewsletterAdmin(admin.ModelAdmin):
+    """Admin class for Newsletter"""
+
+    model = SubscribeNewsletter
+    email_list = ('email', )
+
+admin.site.register(SubscribeNewsletter, SubscribeNewsletterAdmin)
---
+++ newsletter/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+
+
+class SubscribeNewsletter(models.Model):
+    """ Stores email addresses of subscribers """
+
+    email = models.EmailField(
+        max_length=255, null=False, blank=False, unique=True
+    )
+
+    class Meta:
+        """
+        Meta class for SubscribeNewsletter
+        """
+        verbose_name = 'Newsletter Subscribers List'
+
+    def __str__(self):
+        return str(self.email)
